5b0493db61ed076e3e34d531451e5de58217ee3d
Winston McHetz <winstonmchetz@R2Hs-MBP.attlocal.net>
2022-02-07 20:48:08 -0500

    challenged

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-let myName = "Winston"
 
-function greet(x) {
-    console.log(x, myName)
-}
-greet("Hello,")
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--distance');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(function(position){
+    const {latitude} = position.coords;
+    const {longitude} = position.coords;
+    console.log(`https.www.google.pt/maps/@${latitude}, ${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09])
+    .addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+
+}, function() {
+    alert('could not get your position')
+})
